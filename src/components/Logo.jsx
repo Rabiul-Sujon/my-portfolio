1ab77d8d35bc282f9ad32430a676e11f7f483f48
Rabiul-Sujon <alamrabiul889@gmail.com>
2026-07-05 02:02:16 +0600

    fix logo filename

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Logo() {
   return (
     <Image
-      src="/portfolio.logo.png"
+      src="/portfolio-logo.png"
       alt="Rabiul Sujon Logo"
       width={150}
       height={40}
